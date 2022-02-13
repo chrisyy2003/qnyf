ad8d68bbc5686ce9d15e40aa030851281b07f7dd
chrisyang2003 <1017975501@qq.com>
2022-02-13 18:04:03 +0800

    update json

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 # coding=utf-8 
 
-import sqlite3
+import json
 from qnyflib import QNDK
-import requests
 import os
 
 path = os.path.dirname(os.path.abspath(__file__))
-con = sqlite3.connect(path + '/studata.db')
-cur = con.cursor()
+with open(path + '/studata.json', 'rb') as f:
+    studata = json.loads(f.read())
 
-count = 0
-res = list(cur.execute('SELECT * FROM stu'))
-for row in res:
-    num, name, passwd, loc, valid = row
-    if valid:
-        try:
-            # 10623 is your school code
-            stu = QNDK(10623, num, name, passwd, loc)
-            if stu.Daka():
-                count += 1
-                print(name, '打卡成功')
-        except Exception as e:
-            print(name, e)
-    else:
-        print(name, '不打卡')
-        count += 1
+
+for row in studata:
+    num  = row['num']
+    name = row['name']
+    passwd = row['passwd']
+    loc = row['loc']
+
+    try:
+        # 10623 is your school code
+        stu = QNDK(10623, num, name, passwd, loc)
+        print(stu.USRID)
+        
+        # if stu.Daka():
+        #     print(name, '打卡成功')
+    except Exception as e:
+        print(name, e)

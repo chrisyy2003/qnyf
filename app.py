8b7bca0f3210fc1d762aa14ea652eb4361358028
chrisyy <1017975501@qq.com>
2022-11-09 14:46:21 +0800

    Done

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def add():
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', debug=True, port=4000, use_reloader=True)
+    app.run(host='0.0.0.0', debug=True, port=4000, use_reloader=True)
